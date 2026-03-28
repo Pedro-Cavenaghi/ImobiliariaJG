6cca9f9e16fc0280ceaee1b8a5bd7a101295f09c
Pedro-Cavenaghi <luan_trani@hotmail.com>
2026-03-28 11:18:00 -0300

    novo index e conexao com banco

---
+++ config/database.php
@@ -0,0 +1,43 @@
+<?php
+$host = "localhost";
+$dbname = "imobiliariaJG";
+$username = "root";
+$password = "";
+
+class Database {
+    private static $instance = null;
+    private $connection;
+
+    private function __construct()
+    {
+        $host = "localhost";
+        $dbname = "imobiliariaJG";
+        $username = "root";
+        $password = "";
+
+        try {
+      $this->connection = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $username, $password
+    );
+    $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexão realizada com sucesso!<br>";
+    
+} catch (PDOException $e) {
+    die("Erro na conexão: " . $e->getMessage());
+}
+
+    }
+
+    public static function getInstance(){
+    if (self::$instance === null) {
+        self::$instance = new Database();
+    }
+    return self::$instance;
+}
+public function getConnection() {
+    return $this ->connection;
+}
+}
+
+?>
---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+ require_once '../config/database.php';
+
+    $db = Database::getInstance();
+    $conn = $db->getConnection();
+
+    echo "Conexão realizada no index";
+
+    $stmt = $conn->prepare(
+        "INSERT INTO cidades (nome, estado) VALUES (?, ?)"
+    );
+    $stmt->execute(["Itapira", "SP"]);
+
+    $stmt = $conn->query("SELECT * FROM cidades");
+    $cidades = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo "<pre>";
+    print_r($cidades);
+    echo "</pre>";
+?>
